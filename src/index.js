01e7d39a0d19327c2c2362ac75656b22e365f0cd
Andrii Kolisnyk <scear.sic@gmail.com>
2020-11-08 11:50:47 +0100

    #1: Add configuration file with store list and methos for parsing this list

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 const openConfigFile = async () => {
-    const data = await fs.openSync('./index.js');
-    console.log(data);
-    return {}
+    const data = await fs.readFileSync("./stores.json", "utf-8");
+    const json = JSON.parse(data);
+    const list = json.list.map(item => item.url);
+    return list;
 }
 
 const pull = async () => {
+    const data = await openConfigFile();
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto("https://keygem.store", {
+    await page.goto(data[0], {
         waitUntil: "networkidle0"
     });
     await page.screenshot({path: "page.png"})
