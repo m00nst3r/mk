c13394f975067aee847b7dd55552229336d4c3b4
Andrii Kolisnyk <scear.sic@gmail.com>
2020-11-29 12:24:40 +0100

    #1: Fetch all pages and save screenshots

--- src/index.js
+++ src/index.js
@@ -2,22 +2,31 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 const openConfigFile = async () => {
-    const data = await fs.readFileSync("./stores.json", "utf-8");
+    const data = fs.readFileSync("./stores.json", "utf-8");
     const json = JSON.parse(data);
-    const list = json.list.map(item => item.url);
-    return list;
+    return json.list.map(item => item.url);
+}
+
+const fetchPage = async (url, browser) => {
+    const pageName = new URL(url).host;
+    const page = await browser.newPage();
+    await page.goto(url, {
+        waitUntil: "networkidle0"
+    });
+    await page.screenshot({path: `${pageName}.png`});
+    await page.close();
+}
+
+const pullAll = async (data, browser) => {
+    return Promise.all(data.map(url => fetchPage(url, browser)));
 }
 
 const pull = async () => {
     const data = await openConfigFile();
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(data[0], {
-        waitUntil: "networkidle0"
-    });
-    await page.screenshot({path: "page.png"})
 
-    await page.close()
+    await pullAll(data, browser);
+
     await browser.close();
 }
 
